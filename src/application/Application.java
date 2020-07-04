554c6dcc32c0622e4288840686fce1bf74ae5720
Sherika Jones <azshjones12@aol.com>
2020-07-04 07:07:03 -0700

    Update Application.java

--- src/application/Application.java
+++ src/application/Application.java
@@ -5,7 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 		Menu menu = new Menu();
 		menu.start(); 
-//System.out.println("Hello");
+System.out.println("Hello");
 	}
 
 }
